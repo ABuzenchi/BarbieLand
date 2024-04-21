ff9ce86547b60c78d70754065db9a54b639a8a39
alex <147138151+codralexandra@users.noreply.github.com>
2024-04-21 19:59:14 +0300

    Adaugat clasa pentru Texture managing

---
+++ BarbieLand/BarbieLand/Texture.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+#include <GL/glew.h>
+#include <stb_image.h>
+
+
+class Texture
+{
+public:
+	Texture(char const* path);
+	Texture(std::vector<std::string> faces);
+
+	unsigned int loadTexture(char const* path);
+	unsigned int loadCubemapTexture(std::vector<std::string> faces);
+public:
+	unsigned int ID;
+};
+
